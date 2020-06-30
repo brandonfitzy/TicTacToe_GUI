6d2c5f626f81e8077d82d62824a404e8ca16fecf
brandon <brandon.fitzpatrick8154@gmail.com>
2020-06-29 21:18:23 -0500

    playing with javafx

--- src/main/java/com/fitzpatrick/App.java
+++ src/main/java/com/fitzpatrick/App.java
@@ -1,9 +1,11 @@
 package com.fitzpatrick;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 /**
@@ -16,8 +18,15 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        //var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        //var scene = new Scene(new StackPane(label), 640, 480);
+        Line line = new Line();
+        line.setStartX(150.0f);
+        line.setStartY(140.0f);
+        line.setEndX(450.0f);
+        line.setEndY(140.0f);
+        Group root = new Group();
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
